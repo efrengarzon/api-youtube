7ceb7b1e25615752648e512ce09c57c7bd2642dd
efrengarzon <95939840+efrengarzon@users.noreply.github.com>
2022-01-05 14:04:09 -0500

    today

--- src/database.ts
+++ src/database.ts
@@ -6,6 +6,9 @@ import config from './config';
         const mongooseOptions = {
             useUnifiedTopology: true,
             useNewUrlParser: true
+            /* user: config.MONGO_USER,
+            pass: config.MONGO_PASSWORD */
+
         }
         const db = await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`, mongooseOptions);
         console.log('database is  connected')      
